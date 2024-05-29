8c61294219e59005ace696a5715cb719fb4edb27
ddj82 <dnwnrudckf77@gmail.com>
2024-05-29 12:22:11 +0900

    학원114 마지막 업로드 스프링부트예제

---
+++ second/src/main/java/kr/co/biz/board/ResponseDTO.java
@@ -0,0 +1,20 @@
+package kr.co.biz.board;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResponseDTO<T> {
+	private String error;
+	private List<T> data;
+	private Map<T, Object> mapdata;
+}
+
